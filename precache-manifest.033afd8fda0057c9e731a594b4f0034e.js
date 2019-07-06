c1e9015a13edfcd1abdbc8db81cfa469d02be7ed
saravana kumar <ravi.saravna8@gmail.com>
2019-07-06 23:02:12 +0530

    Updates

---
+++ precache-manifest.033afd8fda0057c9e731a594b4f0034e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91b61207b3a00a976c7b3ac2059414cd",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "4bf58e764cbfc122464a",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "0d795e6b1ac594df4a69",
+    "url": "/robofriends/static/css/main.d6a6d6f6.chunk.css"
+  },
+  {
+    "revision": "4bf58e764cbfc122464a",
+    "url": "/robofriends/static/js/2.61b6dae8.chunk.js"
+  },
+  {
+    "revision": "0d795e6b1ac594df4a69",
+    "url": "/robofriends/static/js/main.0275234e.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "4037afe4cfafb6d8c0971e454d471c0e",
+    "url": "/robofriends/static/media/JandaCurlygirlPop.4037afe4.woff"
+  }
+]);
