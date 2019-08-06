584b9207de4bda4539fee42692ba046ea705d119
saravana kumar <ravi.saravna8@gmail.com>
2019-08-06 13:15:36 +0530

    Adding Error Boundary

--- src/Cardlist.js
+++ src/Cardlist.js
@@ -3,8 +3,10 @@ import Card from './Card';
 class Cardlist extends Component{
     render(){
         let robots=[];
-        if(this.props.robots.length===0)
+        if(this.props.robots.length===0){
+            console.log('Hit');
             throw new Error('No Robots Found');
+        }
         for(var i=0;i<this.props.robots.length;i++){
             robots.push(<Card key={i} name={this.props.robots[i]}/>);
         }
